ee75c825fca7f652dbdd53aa4db40661891af93d
srjuninn <ryanferreiradasilva2006@gmail.com>
2025-12-01 09:09:12 -0300

    feat: add home redirect

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -1,4 +1,4 @@
-// Logica do menu hamburguer
+// Lógica do menu hamburguer
 const hamburgerBtn = document.getElementById("hamburgerBtn");
 const navMenu = document.getElementById("navMenu");
 
@@ -6,3 +6,12 @@ hamburgerBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show");
   hamburgerBtn.classList.toggle("active");
 });
+
+// Lógica da imagem que redireciona para /index.html
+const voltarImg = document.getElementById("backToMenu"); // id da imagem no HTML
+
+if (voltarImg) {
+  voltarImg.addEventListener("click", () => {
+    window.location.href = "/index.html";
+  });
+}
